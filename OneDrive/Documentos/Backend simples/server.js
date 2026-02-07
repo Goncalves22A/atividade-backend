6a13ee95fae87532e8d65c6d759c3e41588a6e16
Melissa Goncalves <megoncalves602@gmail.com>
2026-02-07 18:40:38 -0300

    backend simples

---
+++ OneDrive/Documentos/Backend simples/server.js
@@ -0,0 +1,16 @@
+﻿const express = require("express");
+
+const app = express();
+const port = 3000;
+
+// rota GET
+app.get("/", (req, res) => {
+  res.json({
+    mensagem: "hello!"
+  });
+});
+
+// iniciar servidor
+app.listen(port, () => {
+  console.log(`Servidor rodando em http://localhost:${port}`);
+});
